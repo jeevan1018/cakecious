dc473926503934fff684974764e357b717bbff5b
jeevan1018 <jeevanpalapati@gmail.com>
2025-02-11 23:34:15 +0530

    navbar structurecompleted

---
+++ src/Views/Components/Header/Navbar.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import "./Navbar.scss"
+
+const Navbar = () => {
+  return (
+    <div className='header'>
+        <div className="header-left">
+
+            <div>000-111-222 </div>
+            <div>example@example.com</div>
+
+        </div>
+        <div className="header-right">
+            
+        </div>
+      
+    </div>
+  )
+}
+
+export default Navbar
--- src/Views/Home/Index.jsx
+++ src/Views/Home/Index.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import Header from '../Components/Header/Navbar'
 
 const Index = () => {
   return (
     <div>
-      heelo
+      <Header/>
     </div>
   )
 }
